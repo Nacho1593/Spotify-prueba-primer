89d771f77605432e5add755678b01c1c77c6f296
Nacho1593 <jcarricart@gmail.com>
2021-07-17 18:24:42 -0300

    cambios

--- src/components/BrowseView/index.js
+++ src/components/BrowseView/index.js
@@ -3,24 +3,24 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import {
   fetchPlaylistSongs,
-  addPlaylistItem
+  addPlaylistItem,
 } from "../../actions/playlistActions";
 import { updateHeaderTitle } from "../../actions/uiActions";
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     view: state.browseReducer.view,
     viewType: state.songsReducer.viewType,
-    token: state.tokenReducer.token
+    token: state.tokenReducer.token,
   };
 };
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return bindActionCreators(
     {
       fetchPlaylistSongs,
       updateHeaderTitle,
-      addPlaylistItem
+      addPlaylistItem,
     },
     dispatch
   );
